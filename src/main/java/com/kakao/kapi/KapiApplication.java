c5f387172ccbe531850705eaa03fc62a57fe69e3
chaehun <youspend8@gmail.com>
2020-11-19 13:09:17 +0900

    Initial Repository

---
+++ src/main/java/com/kakao/kapi/KapiApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.kapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakao/kapi/KapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.kapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
