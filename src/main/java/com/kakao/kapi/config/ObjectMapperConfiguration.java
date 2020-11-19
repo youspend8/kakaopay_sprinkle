6045bd72ceee58a26eb9c852443a93c253381df3
Imchaehoon <youspend8@gmail.com>
2020-11-19 23:10:36 +0900

    feat : ObjectMapper bean configuration 작성
    - VO -> JSON Format 전환간 Key naming strategy SNAKE_CASE로 설정

---
+++ src/main/java/com/kakao/kapi/config/ObjectMapperConfiguration.java
@@ -0,0 +1,15 @@
+package com.kakao.kapi.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    }
+}
