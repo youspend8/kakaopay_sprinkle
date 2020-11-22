84b61a6340aeb99849e9a014fef5a9e62ec695b7
Imchaehoon <youspend8@gmail.com>
2020-11-22 11:49:10 +0900

    chore : ErrorType CODE_102 추가
    - CODE_102(102, "해당 Token의 뿌리기를 찾을 수 없습니다.")

--- src/main/java/com/kakao/kapi/constants/ErrorType.java
+++ src/main/java/com/kakao/kapi/constants/ErrorType.java
@@ -16,7 +16,8 @@ public enum ErrorType {
     CODE_23(23, "자신이 뿌리기한 건은 자신이 받을 수 없습니다."),
     CODE_50(50, "유효하지 않은 token 입니다."),
     CODE_100(100, "요청 헤더 중 누락된 값이 존재합니다."),
-    CODE_101(101, "Internal Server Error");
+    CODE_101(101, "Internal Server Error"),
+    CODE_102(102, "해당 Token의 뿌리기를 찾을 수 없습니다.");
 
     private final int code;
     private final String message;
